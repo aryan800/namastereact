cd170cd23fc89fe3d9872f9b558240efdffccc92
aryan800 <ak0252224@gmail.com>
2024-01-27 22:16:48 +0530

    Episode 3 completed

--- app.js
+++ app.js
@@ -1,27 +1,28 @@
 import React from "react";
 import ReactDOM  from "react-dom/client";
 
-const parent = React.createElement(
-    "div",
-    { id: "parent" },
-    [React.createElement(
-      "div",
-      { id: "child" },
-      [   React.createElement("h1", {}, "I'm an h1 tag."),
-          React.createElement("h2", {}, "I'm an h2 tag.")
-       ]
-    ),
-    React.createElement(
-      "div",
-      { id: "child2" },
-      [   React.createElement("h1", {}, "I'm an h1 tag."),
-          React.createElement("h2", {}, "I'm an h2 tag.")
-       ]
-    )]
-  );
-  
-  console.log(parent);
-  
-  const root = ReactDOM.createRoot(document.getElementById("root"));
-  
-  root.render(parent);
+// React Element
+const Title = () => (
+    <h1 className="head" tabIndex={5}>
+        Namaste React using JSX
+    </h1>
+);
+
+// React Functional Component - It is a normal JS function which returns some JSX elements.
+
+const HeadingComponent = () => (
+    <div id="container">
+        {Title()}
+        <Title />
+        <Title></Title>
+        <h1>It is a Functional Component.</h1>
+    </div>
+);
+
+// const HeadingComponent = () => {
+//     return <h1>It is a Functional Component.</h1>
+// }
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<HeadingComponent />)
